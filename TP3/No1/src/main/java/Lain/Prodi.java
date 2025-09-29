6590bc252b2f52cf0c1ed5d9f195867efc6d6e66
Kruwpuck <ihabhasanainakmal0409@gmail.com>
2025-09-29 07:28:16 +0700

    TP3

---
+++ TP3/No1/src/main/java/Lain/Prodi.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lain;
+
+/**
+ *
+ * @author ihabh
+ */
+public class Prodi {
+    private String nama;
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getNama(){
+        return this.nama;
+    }
+}
